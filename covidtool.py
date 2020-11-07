f97bd38b21554dcfdaac24ba00cde05366b0ce2d
Florian <leovinci.florian@gmail.com>
2020-11-07 20:54:40 +0100

    added folium support

--- covidtool.py
+++ covidtool.py
@@ -1,5 +1,12 @@
-import json, requests
+import json, requests, folium
 
+print("Loading data...")
 apiurl = 'https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.geojson'
-
 data = requests.get(apiurl).json()
+print("Data loaded!")
+
+m = folium.Map()
+folium.GeoJson(data).add_to(m)
+print("Saving data...")
+m.save("index.html")
+print("Saved data to index.html")
