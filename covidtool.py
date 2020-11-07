728ecef6a46454114994ca9c301426254baaebf2
Florian <leovinci.florian@gmail.com>
2020-11-07 20:05:41 +0100

    switched to geojson

--- covidtool.py
+++ covidtool.py
@@ -1,5 +1,5 @@
 import json, requests
 
-apiurl = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=Bundesland,Landkreis,AnzahlTodesfall,Meldedatum,Datenstand,AnzahlFall&outSR=4326&f=json'
+apiurl = 'https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.geojson'
 
 data = requests.get(apiurl).json()
