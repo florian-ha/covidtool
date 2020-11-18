7cdfb09a7e9795a6650dfbd8e641c25079ce5e7b
Florian <leovinci.florian@gmail.com>
2020-11-18 13:10:41 +0100

    fixed spelling

--- covidtool.py
+++ covidtool.py
@@ -6,7 +6,7 @@ apiurl = 'https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_
 data = requests.get(apiurl).json()
 print("Data loaded!")
 
-m = folium.Map(location=[51.629,10.867], zoom_start=6)
+m = folium.Map(location=[51.629, 10.867], zoom_start=6)
 folium.GeoJson(data).add_to(m)
 print("Saving data...")
 m.save("index.html")
