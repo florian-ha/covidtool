0c55664f3441c89a61c653bc1b33210d49a1201e
Florian-Ha <florian@familie-hasenauer.de>
2020-11-28 16:50:35 +0100

    reformated file

--- coviddata.py
+++ coviddata.py
@@ -1,4 +1,4 @@
-#RKI Corona Landkreise: https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0
+# RKI Corona Landkreise: https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0
 
 import pandas as pd
 import requests
@@ -14,7 +14,7 @@ print("Finished downloading!")
 
 df = pd.read_csv('data.csv')
 df.to_excel(r'data.xlsx', index=False)
-df.sort_values(by=['cases7_per_100k'],inplace=True ,ascending=False)
-casesandnames = df[['GEN','BEZ','BL','cases7_per_100k']]
+df.sort_values(by=['cases7_per_100k'], inplace=True, ascending=False)
+casesandnames = df[['GEN', 'BEZ', 'BL', 'cases7_per_100k']]
 print(casesandnames)
 casesandnames.to_excel(r'casesandnames.xlsx', index=False)
