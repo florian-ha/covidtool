e7e01e404590d52443083e0fa1960176aa27ec2e
Florian-Ha <florian@familie-hasenauer.de>
2020-11-30 15:14:11 +0100

    added comments

--- covidtool.py
+++ covidtool.py
@@ -1,11 +1,13 @@
 import folium
 import requests
 
+# Loads Geojson from RKI Api
 print("Loading data...")
 apiurl = 'https://opendata.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0.geojson'
 data = requests.get(apiurl).json()
 print("Data loaded!")
 
+# Creates and saves new Folium Map with counties in Germany
 m = folium.Map(location=[51.629, 10.867], zoom_start=6)
 folium.GeoJson(data).add_to(m)
 print("Saving data...")
